7fecfa2489fce58bf6701e54489e5a045daccae8
leekelleher <leekelleher@gmail.com>
2019-06-18 16:54:36 +0100

    ByteSize - added field
    to set the number of bytes per kilobyte.

--- src/Our.Umbraco.Contentment/DataEditors/ByteSize/ByteSizeConfigurationEditor.cs
+++ src/Our.Umbraco.Contentment/DataEditors/ByteSize/ByteSizeConfigurationEditor.cs
@@ -11,13 +11,32 @@ namespace Our.Umbraco.Contentment.DataEditors
 {
     internal class ByteSizeConfigurationEditor : ConfigurationEditor
     {
+        public const string Decimals = "decimals";
         public const string Filter = "filter";
         public const string Format = "format";
+        public const string Kilo = "kilo";
 
         public ByteSizeConfigurationEditor()
         {
             Fields.Add(
-                Format,
+                Kilo,
+                "Kilobytes?",
+                "How many bytes are there in a Kilobyte?<br>Kilobyte (1000), kibibyte (1024), you decide.",
+                IOHelper.ResolveUrl(DropdownListDataEditor.DataEditorViewPath),
+                new Dictionary<string, object>
+                {
+                    { AllowEmptyConfigurationField.AllowEmpty, Constants.Values.False },
+                    { DropdownListConfigurationEditor.Items, new[]
+                        {
+                            new DataListItem { Name = "1000 Bytes", Value = "1000" },
+                            new DataListItem { Name = "1024 Bytes", Value = "1024" },
+                        }
+                    },
+                    { DropdownListConfigurationEditor.DefaultValue, "1024" },
+                });
+
+            Fields.Add(
+                Decimals,
                 "Decimal places",
                 "How many decimal places would you like?",
                 IOHelper.ResolveUrl("~/umbraco/views/propertyeditors/slider/slider.html"),
@@ -40,6 +59,18 @@ namespace Our.Umbraco.Contentment.DataEditors
                 // https://github.com/umbraco/Umbraco-CMS/blob/release-8.0.2/src/Umbraco.Web.UI.Client/src/views/propertyeditors/readonlyvalue/readonlyvalue.controller.js#L18-L22
                 // TODO: [LK:2019-06-07] Patch supplied to Umbraco: https://github.com/umbraco/Umbraco-CMS/pull/5615
                 config.Add(Filter, "formatBytes");
+
+                if (config.ContainsKey(Format) == false && config.ContainsKey(Kilo) && config.ContainsKey(Decimals))
+                {
+                    config.Add(Format, new
+                    {
+                        kilo = config[Kilo],
+                        decimals = config[Decimals]
+                    });
+
+                    config.Remove(Kilo);
+                    config.Remove(Decimals);
+                }
             }
 
             return config;
--- src/Our.Umbraco.Contentment/DataEditors/ByteSize/byte-size.js
+++ src/Our.Umbraco.Contentment/DataEditors/ByteSize/byte-size.js
@@ -6,15 +6,15 @@
 angular.module("umbraco.filters").filter("formatBytes", function () {
     // Credit goes to the contributors of this StackOverflow answer:
     // https://stackoverflow.com/a/18650828/12787
-    return function (bytes, decimals) {
-        if (bytes === 0) return "0 Bytes";
+    return function (bytes, opts) {
+        if (bytes === 0 || opts === undefined) return "0 Bytes";
 
-        const k = 1024;
-        const dm = decimals < 0 ? 0 : decimals;
+        const k = parseInt(opts.kilo) || 1024;
+        const dm = parseInt(opts.decimals) || 0;
         const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
         const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + sizes[i];
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
     }
 });
