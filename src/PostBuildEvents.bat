d7261111e59a3f8681206a4b356fd85477a5e65e
leekelleher <leekelleher@gmail.com>
2019-07-09 09:47:28 +0100

    Updated post-build event to remove comments from CSS & JS.
    But you need AjaxMin installed.
    I'll figure out a way to NuGet reference it.

--- src/PostBuildEvents.bat
+++ src/PostBuildEvents.bat
@@ -30,6 +30,8 @@ FOR /R "%4DataEditors\" %%f IN (*.html) DO COPY "%%f" "%WEBSITE_PATH%\App_Plugin
 
 REM Concatenate all the property-editor CSS files into a single file
 FOR /R "%4DataEditors\" %%f IN (*.css) DO TYPE "%%f" >> "%WEBSITE_PATH%\App_Plugins\Contentment\contentment.css"
+AjaxMinifier.exe "%WEBSITE_PATH%\App_Plugins\Contentment\contentment.css" -out "%WEBSITE_PATH%\App_Plugins\Contentment\contentment.css" -pretty
 
 REM Concatenate all the property-editor JS files into a single file
 FOR /R "%4DataEditors\" %%f IN (*.js) DO TYPE "%%f" >> "%WEBSITE_PATH%\App_Plugins\Contentment\contentment.js"
+AjaxMinifier.exe "%WEBSITE_PATH%\App_Plugins\Contentment\contentment.js" -out "%WEBSITE_PATH%\App_Plugins\Contentment\contentment.js" -pretty
