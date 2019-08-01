eb28652557d583247420ac8e30101ea9781d3b4c
leekelleher <leekelleher@gmail.com>
2019-08-01 14:57:34 +0100

    [WIP] Registering Umb package
    Attempting to manually add the package to the "installed packages"
    in the Umbraco back-office.
    
    The scenario behind this is if you install via NuGet. Then the
    back-office has no idea that the package is installed.
    
    By having it recognised as "installed", is that it could be un-installed
    and the "options" page could (potentially) be set too.

--- src/Umbraco.Community.Contentment/Composing/ContentmentComposer.cs
+++ src/Umbraco.Community.Contentment/Composing/ContentmentComposer.cs
@@ -1,18 +1,19 @@
 ﻿/* Copyright © 2019 Lee Kelleher, Umbrella Inc and other contributors.
-* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 using Umbraco.Core;
 using Umbraco.Core.Composing;
 
-namespace Umbraco.Community.Contentment
+namespace Umbraco.Community.Contentment.Composing
 {
     [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
-    public class ContentmentComposer : IUserComposer
+    internal class ContentmentComposer : IUserComposer
     {
         public void Compose(Composition composition)
         {
+            composition.Components().Append<ContentmentComponent>();
         }
     }
 }
---
+++ src/Umbraco.Community.Contentment/Migrations/ContentmentPlan.cs
@@ -0,0 +1,21 @@
+﻿/* Copyright © 2019 Lee Kelleher, Umbrella Inc and other contributors.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using Umbraco.Community.Contentment.Migrations.Install;
+using Umbraco.Core.Migrations;
+
+namespace Umbraco.Community.Contentment.Migrations
+{
+    internal class ContentmentPlan : MigrationPlan
+    {
+        public ContentmentPlan()
+            : base(Constants.Internals.ProjectName)
+        {
+            From(string.Empty)
+                .To<RegisterUmbracoPackageEntry>("{92FFD63F-44DC-4555-A347-87C5B1D24D6E}")
+            ;
+        }
+    }
+}
