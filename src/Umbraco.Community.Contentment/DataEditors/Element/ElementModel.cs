7016bf9cf2bc7d0e73ce6df9f81a3fa6482f2520
leekelleher <leekelleher@gmail.com>
2019-09-02 19:46:47 +0100

    [WIP] IPublishedElement value-converter

---
+++ src/Umbraco.Community.Contentment/DataEditors/Element/ElementModel.cs
@@ -0,0 +1,33 @@
+﻿/* Copyright © 2019 Lee Kelleher, Umbrella Inc and other contributors.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Umbraco.Community.Contentment.DataEditors
+{
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    internal class ElementModel
+    {
+        public Guid ElementType { get; set; }
+
+        public Guid Key { get; set; }
+
+        // TODO: LK:2019-09-02] Need to look for a `string` -> `Udi` JSON converter.
+        // https://github.com/umbraco/Umbraco-CMS/blob/853087a75044b814df458457dc9a1f778cc89749/src/Umbraco.Core/Serialization/UdiJsonConverter.cs
+        // https://github.com/umbraco/Umbraco-CMS/blob/853087a75044b814df458457dc9a1f778cc89749/src/Umbraco.Core/Serialization/KnownTypeUdiJsonConverter.cs
+        // https://github.com/umbraco/Umbraco-CMS/blob/853087a75044b814df458457dc9a1f778cc89749/src/Umbraco.Core/Udi.cs
+        //[JsonConverter(typeof(UdiJsonConverter))]
+        public string Udi { get; set; }
+
+        public string Name { get; set; }
+
+        public string Icon { get; set; }
+
+        public Dictionary<string, object> Value { get; set; }
+    }
+}
---
+++ src/Umbraco.Community.Contentment/Web/PublishedCache/DetachedPublishedElement.cs
@@ -0,0 +1,37 @@
+﻿/* Copyright © 2019 Lee Kelleher, Umbrella Inc and other contributors.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Umbraco.Community.Contentment.Web.PublishedCache
+{
+    internal class DetachedPublishedElement : IPublishedElement
+    {
+        public DetachedPublishedElement(Guid key, IPublishedContentType contentType, IEnumerable<IPublishedProperty> properties)
+        {
+            Key = key;
+            ContentType = contentType;
+            Properties = properties;
+        }
+
+        public IPublishedContentType ContentType { get; }
+
+        public Guid Key { get; }
+
+        public IEnumerable<IPublishedProperty> Properties { get; }
+
+        public IPublishedProperty GetProperty(string alias)
+        {
+            var index = ContentType.GetPropertyIndex(alias);
+
+            return index > 0
+                ? Properties.ElementAtOrDefault(index)
+                : null;
+        }
+    }
+}
