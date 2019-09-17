57aad0a640c6e00008adc5f288c88612da1cabad
leekelleher <leekelleher@gmail.com>
2019-09-17 09:53:48 +0100

    Added Composer stub file (for future use)

---
+++ src/Umbraco.Community.Contentment/Composing/ContentmentComposer.cs
@@ -0,0 +1,18 @@
+﻿/* Copyright © 2019 Lee Kelleher, Umbrella Inc and other contributors.
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace Umbraco.Community.Contentment
+{
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+    public class ContentmentComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+        }
+    }
+}
