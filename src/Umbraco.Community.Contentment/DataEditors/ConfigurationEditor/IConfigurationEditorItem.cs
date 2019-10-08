629021ade19c3136d46a076c9a142d8cbf0734a2
leekelleher <leekelleher@gmail.com>
2019-10-08 11:11:30 +0100

    Tweaks

--- src/Umbraco.Community.Contentment/DataEditors/ConfigurationEditor/IConfigurationEditorItem.cs
+++ src/Umbraco.Community.Contentment/DataEditors/ConfigurationEditor/IConfigurationEditorItem.cs
@@ -3,8 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+using System.ComponentModel;
+
 namespace Umbraco.Community.Contentment.DataEditors
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public interface IConfigurationEditorItem : IContentmentListItem
     { }
 }
