9ef4cdf5f4ac20598ca7b62a474670ed16905e0a
leekelleher <leekelleher@gmail.com>
2019-11-05 11:16:11 +0000

    Replaced the MS-DOS build script with Powershell
    This is a step towards using Powershell to build
    the Umbraco and NuGet package files.
    
    This also minifies the HTML files.

---
+++ build/build-assets.ps1
@@ -0,0 +1,56 @@
+param([string]$SolutionDir,
+      [string]$TargetDir,
+      [string]$ProjectName,
+      [string]$ProjectDir,
+      [string]$TargetDevWebsite,
+      [string]$ConfigurationName);
+
+Write-Host $ConfigurationName;
+
+if ($ConfigurationName -eq 'Debug') {
+  Write-Host $SolutionDir;
+  Write-Host $TargetDir;
+  Write-Host $ProjectName;
+  Write-Host $ProjectDir;
+  Write-Host $TargetDevWebsite;
+}
+
+$targetFolder = "${SolutionDir}..\build\tmp";
+
+# Copy DLL / PDB
+$binFolder = "${targetFolder}\bin";
+if (!(Test-Path -Path $binFolder)) {New-Item -Path $binFolder -Type Directory;}
+Copy-Item -Path "${TargetDir}${ProjectName}.*" -Destination $binFolder;
+
+# Copy package front-end files assets
+$pluginFolder = "${targetFolder}\App_Plugins\Contentment\";
+if (!(Test-Path -Path $pluginFolder)) {New-Item -Path $pluginFolder -Type Directory;}
+Copy-Item -Path "${ProjectDir}Web\UI\App_Plugins\Contentment\*" -Force -Recurse -Destination "${pluginFolder}";
+
+# Load WebMarkupMin (for minification)
+[Reflection.Assembly]::LoadFile("${SolutionDir}..\tools\lib\AdvancedStringBuilder.dll");
+[Reflection.Assembly]::LoadFile("${SolutionDir}..\tools\lib\WebMarkupMin.Core.dll");
+$htmlMinifier = [WebMarkupMin.Core.HtmlMinifier]::new();
+
+# HTML - Copy and Minify (or just remove comments)
+$htmlFiles = Get-ChildItem -Path "${ProjectDir}DataEditors" -Recurse -Force -Include *.html;
+foreach($htmlFile in $htmlFiles){
+  $contents = Get-Content -Path $htmlFile.FullName;
+  $minifiedHtml = $htmlMinifier.Minify($contents).MinifiedContent;
+  Set-Content -Path "${pluginFolder}\editors\$($htmlFile.Name)" -Value $minifiedHtml;
+}
+
+# CSS - Bundle & Minify
+$targetCssPath = "${pluginFolder}contentment.css";
+Get-Content -Path "${ProjectDir}DataEditors\**\*.css" | Set-Content -Path $targetCssPath;
+& "${SolutionDir}..\tools\AjaxMinifier.exe" $targetCssPath -o $targetCssPath
+
+# JS - Bundle & Minify
+$targetJsPath = "${pluginFolder}contentment.js";
+Get-Content -Path "${ProjectDir}DataEditors\**\*.js" | Set-Content -Path $targetJsPath;
+& "${SolutionDir}..\tools\AjaxMinifier.exe" $targetJsPath -o $targetJsPath
+
+# In debug mode, copy the assets over to the local dev website
+if ($ConfigurationName -eq 'Debug' -AND -NOT($TargetDevWebsite -eq '')) {
+  Copy-Item -Path "${targetFolder}\*" -Force -Recurse -Destination $TargetDevWebsite;
+}
