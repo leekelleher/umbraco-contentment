3f96378ed57ec77c403660412655417fe238e4f2
René Truelsen <rtr@rtr.dk>
2019-12-02 14:17:53 +0100

    Added xml namespace handling and better logging
    Bad logging is so annoying when trying to figure out the XPath or if XML content is loaded correctly :)

--- src/Umbraco.Community.Contentment/DataEditors/DataList/DataSources/XmlDataListSource.cs
+++ src/Umbraco.Community.Contentment/DataEditors/DataList/DataSources/XmlDataListSource.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright © 2019 Lee Kelleher.
+/* Copyright © 2019 Lee Kelleher.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
@@ -6,7 +6,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Text;
 using System.Xml;
+using System.Xml.XPath;
 using Umbraco.Core;
 using Umbraco.Core.Composing;
 using Umbraco.Core.IO;
@@ -67,9 +69,18 @@ namespace Umbraco.Community.Contentment.DataEditors
             var doc = GetXml();
 
             if (doc == null || string.IsNullOrWhiteSpace(ItemsXPath))
+            {
                 return items;
+            }
 
-            var nodes = doc.SelectNodes(ItemsXPath);
+            var ns = CreateNamespaceManager(doc);
+            var nodes = doc.SelectNodes(ItemsXPath, ns);
+
+            if (nodes.Count == 0)
+            {
+                _logger.Warn<string>("Did not recognize any items in the XML: " + doc.OuterXml);
+                return items;
+            }
 
             var nameXPath = string.IsNullOrWhiteSpace(NameXPath) == false
                 ? NameXPath
@@ -81,8 +92,8 @@ namespace Umbraco.Community.Contentment.DataEditors
 
             foreach (XmlNode node in nodes)
             {
-                var name = node.SelectSingleNode(nameXPath);
-                var value = node.SelectSingleNode(valueXPath);
+                var name = node.SelectSingleNode(nameXPath, ns);
+                var value = node.SelectSingleNode(valueXPath, ns);
 
                 if (name != null && value != null)
                 {
@@ -102,6 +113,12 @@ namespace Umbraco.Community.Contentment.DataEditors
                         Value = value.Value
                     });
                 }
+                else
+                {
+                    _logger.Warn<string>("Did not recognize a name or a value in the node XML: " + node.OuterXml.Substring(0,1000));
+                    _logger.Info<string>($"Result of name XPATH ({NameXPath}): " + (name != null ? name.OuterXml : "null") );
+                    _logger.Info<string>($"Result of value XPATH ({ValueXPath}): " + (value != null ? value.OuterXml : "null") );
+                }
             }
 
             return items;
@@ -120,9 +137,6 @@ namespace Umbraco.Community.Contentment.DataEditors
                 {
                     using (var client = new WebClient())
                     {
-                        doc.LoadXml(client.DownloadString(Url));
-                    }
-                }
                 catch (WebException ex)
                 {
                     _logger.Error<XmlDataListSource>(ex, "Unable to fetch remote data.");
@@ -153,6 +167,36 @@ namespace Umbraco.Community.Contentment.DataEditors
 <p>If you need assistance with XPath syntax, please refer to this resource: <a href=""https://www.w3schools.com/xml/xpath_intro.asp"" target=""_blank""><strong>w3schools.com/xml</strong></a>.</p>
 </div>", true)
             { }
+        }
+
+
+        /// <summary>
+        /// Returns an instance of XmlNamespaceManager with a specified XmlDocument.NameTable.  
+        /// Returns null if no namespace is defined.
+        /// </summary>
+        /// <param name="doc">The XmlDocument</param>
+        /// <returns>XmlNamespaceManager if there is at least one namespace, null if there is no namespace defined.</returns>
+        private XmlNamespaceManager CreateNamespaceManager(XmlDocument doc)
+        {
+            var result = new XmlNamespaceManager(doc.NameTable);
+
+            IDictionary<string, string> localNamespaces = null;
+            XPathNavigator xNav = doc.CreateNavigator();
+            while (xNav.MoveToFollowing(XPathNodeType.Element))
+            {
+                localNamespaces = xNav.GetNamespacesInScope(XmlNamespaceScope.Local);
+                foreach (var localNamespace in localNamespaces)
+                {
+                    string prefix = localNamespace.Key;
+                    if (string.IsNullOrEmpty(prefix))
+                        prefix = "DEFAULT";
+
+                    _logger.Debug<string>("Found and added XML namespace: " + prefix + " : " + localNamespace.Value);
+                    result.AddNamespace(prefix, localNamespace.Value);
+                }
+            }
+
+            return result;
         }
     }
 }
