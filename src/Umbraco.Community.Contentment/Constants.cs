5ad1fe34e5a9af2a28e81bfe261a73b1438e0fd5
leekelleher <leekelleher@gmail.com>
2019-12-18 09:55:20 +0000

    Updated minimum supported Umbraco version number

--- src/Umbraco.Community.Contentment/Constants.cs
+++ src/Umbraco.Community.Contentment/Constants.cs
@@ -50,7 +50,7 @@ namespace Umbraco.Community.Contentment
 
             public const string LicenseUrl = "https://mozilla.org/MPL/2.0/";
 
-            public static readonly System.Version MinimumSupportedUmbracoVersion = new System.Version(8, 2, 0);
+            public static readonly System.Version MinimumSupportedUmbracoVersion = new System.Version(8, 4, 0);
 
             public const string RepositoryUrl = "https://github.com/leekelleher/umbraco-contentment";
         }
