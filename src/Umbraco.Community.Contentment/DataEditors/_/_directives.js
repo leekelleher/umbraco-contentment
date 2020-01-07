d774a59e51cdff32c46608ccdd0a1bd891c3ba82
leekelleher <leekelleher@gmail.com>
2020-01-07 17:24:20 +0000

    Updated directive namespace

--- src/Umbraco.Community.Contentment/DataEditors/_/_directives.js
+++ src/Umbraco.Community.Contentment/DataEditors/_/_directives.js
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-angular.module("umbraco.directives").directive("umbHtmlAttributes", [
+angular.module("umbraco.directives.html").directive("umbHtmlAttributes", [
     function () {
         return {
             restrict: "A",
