2f67c2a8d5ec8336bbfbab6b070fa8e016523e74
leekelleher <leekelleher@gmail.com>
2020-02-14 17:36:12 +0000

    Added "Buttons" editor for Data List
    I've been wanting to do this for such a long time!

---
+++ src/Umbraco.Community.Contentment/DataEditors/Buttons/ButtonsDataListEditor.cs
@@ -0,0 +1,33 @@
+﻿/* Copyright © 2020 Lee Kelleher.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using System.Collections.Generic;
+using Umbraco.Core.IO;
+using Umbraco.Core.PropertyEditors;
+
+namespace Umbraco.Community.Contentment.DataEditors
+{
+    [Core.Composing.HideFromTypeFinder]
+    public sealed class ButtonsDataListEditor : IDataListEditor
+    {
+        public string Name => "Buttons";
+
+        public string Description => "Select multiple values from a group of buttons.";
+
+        public string Icon => "icon-tab";
+
+        public IEnumerable<ConfigurationField> Fields => new ConfigurationField[]
+        {
+            new DefaultIconConfigurationField(),
+            new EnableMultipleConfigurationField()
+        };
+
+        public Dictionary<string, object> DefaultValues => default;
+
+        public Dictionary<string, object> DefaultConfig => default;
+
+        public string View => IOHelper.ResolveUrl(Constants.Internals.EditorsPathRoot + "buttons.html");
+    }
+}
---
+++ src/Umbraco.Community.Contentment/DataEditors/Buttons/buttons.js
@@ -0,0 +1,73 @@
+﻿/* Copyright © 2020 Lee Kelleher.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+angular.module("umbraco").controller("Umbraco.Community.Contentment.DataEditors.Buttons.Controller", [
+    "$scope",
+    function ($scope) {
+
+        // console.log("buttons.model", $scope.model);
+
+        var defaultConfig = {
+            defaultIcon: "icon-science",
+            defaultValue: [],
+            items: [],
+            enableMultiple: 0
+        };
+        var config = angular.extend({}, defaultConfig, $scope.model.config);
+
+        var vm = this;
+
+        function init() {
+            $scope.model.value = $scope.model.value || config.defaultValue;
+
+            if (_.isArray($scope.model.value) === false) {
+                $scope.model.value = [$scope.model.value];
+            }
+
+            vm.icon = config.defaultIcon;
+            vm.multiple = Object.toBoolean(config.enableMultiple);
+
+            if (vm.multiple === false && $scope.model.value.length > 0) {
+                $scope.model.value.splice(1);
+            }
+
+            vm.items = angular.copy(config.items);
+
+            _.each(vm.items, function (item) {
+                item.selected = _.contains($scope.model.value, item.value);
+            });
+
+            vm.select = select;
+        };
+
+        function select(item) {
+
+            item.selected = item.selected === false;
+            $scope.model.value = [];
+
+            _.each(vm.items, function (x) {
+
+                if (vm.multiple === false) {
+                    x.selected = x.value === item.value;
+                }
+
+                if (x.selected) {
+                    $scope.model.value.push(x.value);
+                }
+
+            });
+
+            setDirty();
+        };
+
+        function setDirty() {
+            if ($scope.propertyForm) {
+                $scope.propertyForm.$setDirty();
+            }
+        };
+
+        init();
+    }
+]);
