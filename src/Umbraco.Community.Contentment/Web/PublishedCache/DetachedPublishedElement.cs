9644c0d64da78f6db8ab0f9e51801033de9e1cf7
leekelleher <leekelleher@gmail.com>
2020-05-28 17:50:19 +0100

    Added the fake `IPublishedElement` back in for v1.1.0
    This reverts commit 0a11c025d88eca8b4d66f68221565156853fa641.

---
+++ src/Umbraco.Community.Contentment/Web/PublishedCache/DetachedPublishedElement.cs
@@ -0,0 +1,44 @@
+﻿/* Copyright © 2016 UMCO, Our Umbraco and other contributors.
+ * This Source Code has been derived from Inner Content.
+ * https://github.com/umco/umbraco-inner-content/blob/2.0.4/src/Our.Umbraco.InnerContent/Models/DetachedPublishedContent.cs
+ * Modified under the permissions of the MIT License.
+ * Modifications are licensed under the Mozilla Public License.
+ * Copyright © 2019 Lee Kelleher.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Umbraco.Community.Contentment.Web.PublishedCache
+{
+    internal sealed class DetachedPublishedElement : IPublishedElement
+    {
+        private readonly Dictionary<string, IPublishedProperty> _propertyLookup;
+
+        public DetachedPublishedElement(Guid key, IPublishedContentType contentType, IEnumerable<IPublishedProperty> properties)
+        {
+            Key = key;
+            ContentType = contentType;
+            Properties = properties;
+
+            _propertyLookup = properties.ToDictionary(x => x.Alias, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public IPublishedContentType ContentType { get; }
+
+        public Guid Key { get; }
+
+        public IEnumerable<IPublishedProperty> Properties { get; }
+
+        public IPublishedProperty GetProperty(string alias)
+        {
+            return _propertyLookup.ContainsKey(alias)
+                ? _propertyLookup[alias]
+                : null;
+        }
+    }
+}
