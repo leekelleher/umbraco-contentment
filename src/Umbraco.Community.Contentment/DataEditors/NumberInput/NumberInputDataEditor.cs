99889f966b8ce4533fbdb99b09b2ed143e7ab9ce
leekelleher <leekelleher@gmail.com>
2020-05-28 18:07:32 +0100

    Added "Number Input" back in for v1.2.0
    This reverts commits:
    - f9c039a621e8743e1d1c7949d0481367f4245144
    - b821b4b8966540d998f3b1e2cbc8f3fdf0297e92

--- src/Umbraco.Community.Contentment/DataEditors/NumberInput/NumberInputDataEditor.cs
+++ src/Umbraco.Community.Contentment/DataEditors/NumberInput/NumberInputDataEditor.cs
@@ -3,10 +3,31 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+using Umbraco.Core.Logging;
+using Umbraco.Core.PropertyEditors;
+
 namespace Umbraco.Community.Contentment.DataEditors
 {
-    internal sealed class NumberInputDataEditor
+    [DataEditor(
+        DataEditorAlias,
+        EditorType.PropertyValue,
+        DataEditorName,
+        DataEditorViewPath,
+        ValueType = ValueTypes.Integer,
+        Group = Core.Constants.PropertyEditors.Groups.Common,
+        Icon = DataEditorIcon)]
+    [Core.Composing.HideFromTypeFinder]
+    internal sealed class NumberInputDataEditor : DataEditor
     {
+        internal const string DataEditorAlias = Constants.Internals.DataEditorAliasPrefix + "NumberInput";
+        internal const string DataEditorName = Constants.Internals.DataEditorNamePrefix + "Number Input";
         internal const string DataEditorViewPath = Constants.Internals.EditorsPathRoot + "number-input.html";
+        internal const string DataEditorIcon = "icon-coin";
+
+        public NumberInputDataEditor(ILogger logger)
+            : base(logger)
+        { }
+
+        // TODO: [LK:2020-01-02] Add config options for "size: `umb-property-editor-tiny`, `umb-property-editor-small`, `umb-property-editor--limit-width`, or standard full width".
     }
 }
---
+++ src/Umbraco.Community.Contentment/DataEditors/NumberInput/NumberInputValueConverter.cs
@@ -0,0 +1,17 @@
+﻿/* Copyright © 2020 Lee Kelleher.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors.ValueConverters;
+
+namespace Umbraco.Community.Contentment.DataEditors
+{
+    [Core.Composing.HideFromTypeFinder]
+    internal sealed class NumberInputValueConverter : IntegerValueConverter
+    {
+        public override bool IsConverter(IPublishedPropertyType propertyType) => propertyType.EditorAlias.InvariantEquals(NumberInputDataEditor.DataEditorAlias);
+    }
+}
