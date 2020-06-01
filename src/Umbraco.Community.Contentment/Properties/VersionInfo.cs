bf7e452a94dd3688a6417b84502eea0b461aa657
leekelleher <leekelleher@gmail.com>
2020-06-01 17:39:53 +0100

    Bumped version number for assembly
    I'd forgot to do it when I first branched off.

--- src/Umbraco.Community.Contentment/Properties/VersionInfo.cs
+++ src/Umbraco.Community.Contentment/Properties/VersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-develop")]
+[assembly: AssemblyVersion("1.1")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0-develop")]
