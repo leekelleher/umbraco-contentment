9c6c6f1b18756fa2b415c9d63086c0dbb6387f2a
Anders Bjerner <abjerner@skybrud.dk>
2020-06-24 11:35:45 +0100

    Combines Composer classes, sets RuntimeLevel to Boot
    Fixes #27 (Package breaks during Umbraco upgrade) Thanks @abjerner

--- src/Umbraco.Community.Contentment/Composing/ContentmentComponentComposer.cs
+++ src/Umbraco.Community.Contentment/Composing/ContentmentComponentComposer.cs
@@ -3,16 +3,26 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+using Umbraco.Community.Contentment.DataEditors;
 using Umbraco.Core;
 using Umbraco.Core.Composing;
+using Umbraco.Web.Runtime;
 
 namespace Umbraco.Community.Contentment.Composing
 {
-    [ComposeAfter(typeof(ContentmentBootComposer))]
-    internal sealed class ContentmentRunComposer : IUserComposer
+    [ComposeAfter(typeof(WebInitialComposer))]
+    [RuntimeLevel(MinLevel = RuntimeLevel.Boot)]
+    internal sealed class ContentmentComposer : IUserComposer
     {
         public void Compose(Composition composition)
         {
+            composition
+                .ContentmentListItems()
+                    .Add(() => composition.TypeLoader.GetTypes<IContentmentListItem>())
+            ;
+
+            composition.RegisterUnique<ConfigurationEditorUtility>();
+
             composition
                 .Components()
                     .Append<ContentmentComponent>()
