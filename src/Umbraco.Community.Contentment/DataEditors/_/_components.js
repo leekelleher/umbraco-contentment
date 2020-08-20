b6058af1e6dbf4b2a83166e3e758f102bdabf672
leekelleher <leekelleher@gmail.com>
2020-08-20 14:30:17 +0100

    ContentBlocks - "Cards" display mode

--- src/Umbraco.Community.Contentment/DataEditors/_/_components.js
+++ src/Umbraco.Community.Contentment/DataEditors/_/_components.js
@@ -454,3 +454,149 @@ angular.module("umbraco.directives").component("contentmentBlocksEditor", {
 
         }]
 });
+
+angular.module("umbraco.directives").component("contentmentCardsEditor", {
+    templateUrl: "/App_Plugins/Contentment/components/cards-editor.html",
+    bindings: {
+        addButtonLabel: "@?",
+        addButtonLabelKey: "<?",
+        allowAdd: "<?",
+        allowEdit: "<?",
+        allowRemove: "<?",
+        allowSort: "<?",
+        blockActions: "<?",
+        defaultIcon: "<?",
+        getItemIcon: "<?",
+        getItemName: "<?",
+        ngModel: "=?",
+        onAdd: "<?",
+        onEdit: "<?",
+        onRemove: "<?",
+        onSort: "<?",
+        propertyActions: "<?",
+        previews: "<?",
+    },
+    require: {
+        propertyForm: "^form",
+        umbProperty: "^"
+    },
+    controllerAs: "vm",
+    controller: [
+        "$scope",
+        "localizationService",
+        function ($scope, localizationService) {
+
+            // console.log("contentmentCardsEditor", $scope.vm);
+
+            var vm = this;
+
+            vm.$onInit = function () {
+
+                vm.propertyAlias = vm.umbProperty.property.alias;
+
+                vm.sortableOptions = {
+                    axis: false,
+                    containment: "parent",
+                    cursor: "move",
+                    disabled: vm.allowSort === false,
+                    opacity: 0.7,
+                    scroll: true,
+                    tolerance: "pointer",
+                    stop: function (e, ui) {
+
+                        if (vm.onSort) {
+                            vm.onSort();
+                        }
+
+                        if (vm.propertyForm) {
+                            vm.propertyForm.$setDirty();
+                        }
+                    }
+                };
+
+                vm.add = add;
+                vm.canEdit = canEdit;
+                vm.canRemove = canRemove;
+                vm.edit = edit;
+                vm.populate = populate;
+                vm.remove = remove;
+
+                if (vm.addButtonLabelKey) {
+                    localizationService.localize(vm.addButtonLabelKey).then(function (label) {
+                        vm.addButtonLabel = label;
+                    });
+                }
+
+                if (vm.propertyActions && vm.propertyActions.length > 0) {
+                    vm.umbProperty.setPropertyActions(vm.propertyActions);
+                }
+            };
+
+            function add() {
+                if (typeof (vm.onAdd) === "function") {
+                    vm.onAdd();
+                }
+            };
+
+            function canEdit(item, $index) {
+                switch (typeof (vm.allowEdit)) {
+                    case "boolean":
+                        return vm.allowEdit;
+                    case "function":
+                        return vm.allowEdit(item, $index);
+                    default:
+                        return true;
+                }
+            };
+
+            function canRemove(item, $index) {
+                switch (typeof (vm.allowRemove)) {
+                    case "boolean":
+                        return vm.allowRemove;
+                    case "function":
+                        return vm.allowRemove(item, $index);
+                    default:
+                        return true;
+                }
+            };
+
+            function edit($index) {
+                if (typeof (vm.onEdit) === "function") {
+                    vm.onEdit($index);
+                }
+            };
+
+            function populate(item, $index, propertyName) {
+                if (typeof (vm.getItem) === "function") {
+                    return vm.getItem(item, $index, propertyName);
+                }
+
+                switch (propertyName) {
+                    case "icon":
+                        return typeof (vm.getItemIcon) === "function"
+                            ? vm.getItemIcon(item, $index)
+                            : item.icon || vm.defaultIcon;
+
+                    case "name":
+                        return typeof (vm.getItemName) === "function"
+                            ? vm.getItemName(item, $index)
+                            : item.name;
+
+                    case "description":
+                        return typeof (vm.getItemDescription) === "function"
+                            ? vm.getItemDescription(item, $index)
+                            : item.description;
+
+                    default:
+                        return item[propertyName];
+                }
+            };
+
+            function remove($index) {
+                if (typeof (vm.onRemove) === "function") {
+                    vm.onRemove($index);
+                }
+            };
+
+        }]
+});
