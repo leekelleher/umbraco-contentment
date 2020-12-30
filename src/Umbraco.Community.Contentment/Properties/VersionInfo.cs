5811295120acf8c8805cec9810f1347e2aa1aa0b
leekelleher <leekelleher@gmail.com>
2020-12-30 16:08:11 +0000

    Incremented version number, v1.1.3

--- src/Umbraco.Community.Contentment/Properties/VersionInfo.cs
+++ src/Umbraco.Community.Contentment/Properties/VersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("1.1")]
-[assembly: AssemblyFileVersion("1.1.2")]
-[assembly: AssemblyInformationalVersion("1.1.2-develop")]
+[assembly: AssemblyFileVersion("1.1.3")]
+[assembly: AssemblyInformationalVersion("1.1.3-develop")]
