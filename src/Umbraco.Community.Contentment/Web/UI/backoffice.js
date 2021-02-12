984d897e2465bc1502a4a6aa4273c8b24c0131d5
leekelleher <leekelleher@gmail.com>
2021-02-12 20:40:30 +0000

    Adds telemetry for DataTypes
    When a DataType is saved, a request is sent to my web-server with
    minimal data about the property-editor used.
    
    Further information and analysis is available at:
    https://leekelleher.com/umbraco/contentment/telemetry/

--- src/Umbraco.Community.Contentment/Web/UI/backoffice.js
+++ src/Umbraco.Community.Contentment/Web/UI/backoffice.js
@@ -67,6 +67,19 @@ angular.module("umbraco").controller("Umbraco.Community.Contentment.Tree.Control
             };
 
             vm.csharp = "csharp";
+            vm.telemetryEnabled = config.telemetry === true;
+            vm.disableTelemetryCode = `using Umbraco.Core.Composing;
+
+namespace Our.Umbraco.Web
+{
+    public class DisableContentmentTelemetryComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.DisableContentmentTelemetry();
+        }
+    }
+}`;
             vm.disableTreeCode = `using Umbraco.Core.Composing;
 
 namespace Our.Umbraco.Web
