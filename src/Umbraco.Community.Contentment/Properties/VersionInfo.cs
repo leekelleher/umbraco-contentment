5177ef1dbd387d874005e428bbd5b954b084df30
leekelleher <leekelleher@gmail.com>
2021-03-05 14:57:22 +0000

    Bumped AssemblyInfo version, v1.2.0

--- src/Umbraco.Community.Contentment/Properties/VersionInfo.cs
+++ src/Umbraco.Community.Contentment/Properties/VersionInfo.cs
@@ -2,4 +2,4 @@
 
 [assembly: AssemblyVersion("1.2")]
 [assembly: AssemblyFileVersion("1.2.0")]
-[assembly: AssemblyInformationalVersion("1.2.0-develop")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
