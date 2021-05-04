10d3e4fc617627b9df10ee3f26b58f23a70e9d4c
leekelleher <leekelleher@gmail.com>
2021-05-04 17:42:49 +0100

    Removed the Assembly attribute classes
    No longer required.

--- src/Umbraco.Community.Contentment/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿//using System.Reflection;
-//using System.Runtime.InteropServices;
-//using Umbraco.Community.Contentment;
-
-//[assembly: AssemblyTitle(Constants.Internals.ProjectNamespace)]
-//[assembly: AssemblyDescription("Umbraco Contentment - a state of happiness and satisfaction")]
-
-//[assembly: Guid("7D440D19-44D0-474F-8BAB-A7DDF4062994")]
