60ef6eb1d87afb5be52aac206706e099834fa62f
leekelleher <leekelleher@gmail.com>
2021-05-04 12:35:39 +0100

    PublishedContentContractResolver - commented out deprecated properties
    Will need to investigate how to populate them later.
    e.g. `.Url()` is an important one

--- src/Umbraco.Community.Contentment/Web/Serialization/PublishedContentContractResolver.cs
+++ src/Umbraco.Community.Contentment/Web/Serialization/PublishedContentContractResolver.cs
@@ -73,9 +73,10 @@ namespace Umbraco.Community.Contentment.Web.Serialization
             _systemProperties = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
             {
                 nameof(IPublishedContent.CreateDate),
-#pragma warning disable CS0618 // Type or member is obsolete
-                nameof(IPublishedContent.CreatorName),
-#pragma warning restore CS0618 // Type or member is obsolete
+// TODO: [LK:2021-04-30] v9 Review this.
+//#pragma warning disable CS0618 // Type or member is obsolete
+//                nameof(IPublishedContent.CreatorName),
+//#pragma warning restore CS0618 // Type or member is obsolete
                 nameof(IPublishedContent.Id),
                 nameof(IPublishedContent.ItemType),
                 nameof(IPublishedElement.Key),
@@ -84,13 +85,15 @@ namespace Umbraco.Community.Contentment.Web.Serialization
                 nameof(IPublishedContent.Path),
                 nameof(IPublishedContent.SortOrder),
                 nameof(IPublishedContent.UpdateDate),
-#pragma warning disable CS0618 // Type or member is obsolete
-                nameof(IPublishedContent.Url),
-#pragma warning restore CS0618 // Type or member is obsolete
+// TODO: [LK:2021-04-30] v9 Review this.
+//#pragma warning disable CS0618 // Type or member is obsolete
+//                nameof(IPublishedContent.Url),
+//#pragma warning restore CS0618 // Type or member is obsolete
                 nameof(IPublishedContent.UrlSegment),
-#pragma warning disable CS0618 // Type or member is obsolete
-                nameof(IPublishedContent.WriterName),
-#pragma warning restore CS0618 // Type or member is obsolete
+// TODO: [LK:2021-04-30] v9 Review this.
+//#pragma warning disable CS0618 // Type or member is obsolete
+//                nameof(IPublishedContent.WriterName),
+//#pragma warning restore CS0618 // Type or member is obsolete
             };
         }
 
