d9b0e063a3106f5c8e4276fca0c1ae79eb65011f
leekelleher <leekelleher@gmail.com>
2021-05-11 15:29:34 +0100

    Re-enabled the `DisableContentmentTree()` extension

--- src/Umbraco.Community.Contentment/Trees/CompositionExtensions.cs
+++ src/Umbraco.Community.Contentment/Trees/CompositionExtensions.cs
@@ -4,24 +4,20 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Web;
 using Umbraco.Community.Contentment.Trees;
 using Umbraco.Extensions;
-using Umbraco.Web;
 
-// NOTE: This extension method class is deliberately using the Umbraco namespace,
-// as to reduce namespace imports and ease the developer experience. [LK]
 namespace Umbraco.Core.Composing
 {
     public static partial class CompositionExtensions
     {
         public static IUmbracoBuilder DisableContentmentTree(this IUmbracoBuilder builder)
         {
-            // TODO: [LK:2021-05-03] Commented out, as I had to comment out `ContentmentTreeController` for now.
-
-            //builder
-            //    .Trees()
-            //        .RemoveTreeController<ContentmentTreeController>()
-            //;
+            builder
+                .Trees()
+                    .RemoveTreeController<ContentmentTreeController>()
+            ;
 
             return builder;
         }
