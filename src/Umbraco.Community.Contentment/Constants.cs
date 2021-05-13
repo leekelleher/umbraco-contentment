e54dca55946f9c7878ab3e26214dbc22dc6bef83
leekelleher <leekelleher@gmail.com>
2021-05-13 14:35:05 +0100

    Constants - temporarily removed leading tilde.
    There's a regression issue with Umbraco v9 for when using
    the `DataEditorAttribute`.
    https://github.com/umbraco/Umbraco-CMS/issues/10265
    
    This currently means that Contentment will not currently
    work within virtual folders.

--- src/Umbraco.Community.Contentment/Constants.cs
+++ src/Umbraco.Community.Contentment/Constants.cs
@@ -21,7 +21,7 @@ namespace Umbraco.Community.Contentment
 
             internal const string EditorsPathRoot = PackagePathRoot + "editors/";
 
-            internal const string PackagePathRoot = "~/App_Plugins/" + ProjectName + "/";
+            internal const string PackagePathRoot = "/App_Plugins/" + ProjectName + "/"; // TODO: [v9] [LK:2021-05-11] Add the ~/ back in later.
 
             internal const string PluginControllerName = ProjectName;
 
