89ce4d50aa920142f2723254384741e044cb8149
leekelleher <leekelleher@gmail.com>
2021-07-08 11:44:14 +0100

    Upgraded Umbraco.Cms dependency to v9.0.0-rc001
    Resolved any API breaking-changes.

--- src/Umbraco.Community.Contentment/Composing/ContentmentComponent.cs
+++ src/Umbraco.Community.Contentment/Composing/ContentmentComponent.cs
@@ -3,11 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-using Microsoft.Extensions.Logging;
 using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Migrations;
 using Umbraco.Cms.Core.Scoping;
 using Umbraco.Cms.Core.Services;
-using Umbraco.Cms.Infrastructure.Migrations;
 using Umbraco.Cms.Infrastructure.Migrations.Upgrade;
 using Umbraco.Community.Contentment.Migrations;
 
@@ -15,30 +14,24 @@ namespace Umbraco.Community.Contentment.Composing
 {
     internal sealed class ContentmentComponent : IComponent
     {
+        private readonly IMigrationPlanExecutor _migrationPlanExecutor;
         private readonly IScopeProvider _scopeProvider;
-        private readonly IMigrationBuilder _migrationBuilder;
         private readonly IKeyValueService _keyValueService;
-        private readonly ILogger<Upgrader> _logger;
-        private readonly ILoggerFactory _loggerFactory;
 
         public ContentmentComponent(
+            IMigrationPlanExecutor migrationPlanExecutor,
             IScopeProvider scopeProvider,
-            IMigrationBuilder migrationBuilder,
-            IKeyValueService keyValueService,
-            ILogger<Upgrader> logger,
-            ILoggerFactory loggerFactory)
+            IKeyValueService keyValueService)
         {
+            _migrationPlanExecutor = migrationPlanExecutor;
             _scopeProvider = scopeProvider;
-            _migrationBuilder = migrationBuilder;
             _keyValueService = keyValueService;
-            _logger = logger;
-            _loggerFactory = loggerFactory;
         }
 
         public void Initialize()
         {
             var upgrader = new Upgrader(new ContentmentPlan());
-            upgrader.Execute(_scopeProvider, _migrationBuilder, _keyValueService, _logger, _loggerFactory);
+            upgrader.Execute(_migrationPlanExecutor, _scopeProvider, _keyValueService);
         }
 
         public void Terminate()
--- src/Umbraco.Community.Contentment/Migrations/Install/RegisterUmbracoPackageEntry.cs
+++ src/Umbraco.Community.Contentment/Migrations/Install/RegisterUmbracoPackageEntry.cs
@@ -3,46 +3,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-using System.Linq;
-using Umbraco.Cms.Core.Packaging;
-using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Infrastructure.Migrations;
 
 namespace Umbraco.Community.Contentment.Migrations
 {
     internal sealed class RegisterUmbracoPackageEntry : MigrationBase
     {
-        private readonly IPackagingService _packagingService;
-
         public const string State = "{contentment-reg-umb-pkg-entry}";
 
-        public RegisterUmbracoPackageEntry(IPackagingService packagingService, IMigrationContext context)
+        public RegisterUmbracoPackageEntry(IMigrationContext context)
             : base(context)
-        {
-            _packagingService = packagingService;
-        }
+        { }
 
-        public override void Migrate()
+        protected override void Migrate()
         {
-            // Check if the package has already been installed.
-            var pkgs = _packagingService.GetInstalledPackageByName(Constants.Internals.ProjectName);
-            if (pkgs?.Any() == false)
-            {
-                // If not, then make a package definition and save it to the "installedPackages.config".
-                _packagingService.SaveInstalledPackage(new PackageDefinition
-                {
-                    Name = Constants.Internals.ProjectName,
-                    Url = Constants.Package.RepositoryUrl,
-                    Author = Constants.Package.Author,
-                    AuthorUrl = Constants.Package.AuthorUrl,
-                    IconUrl = Constants.Package.IconUrl,
-                    License = Constants.Package.License,
-                    LicenseUrl = Constants.Package.LicenseUrl,
-                    UmbracoVersion = Constants.Package.MinimumSupportedUmbracoVersion,
-                    Version = ContentmentVersion.Version.ToString(),
-                    Readme = "",
-                });
-            }
+            // NOTE: This migration does nothing. It is a left over from code targeting Umbraco 8.
+            // It needs to remain, as Umbraco instances that have been upgraded from v8 to v9 will have reached this migration state.
         }
     }
 }
