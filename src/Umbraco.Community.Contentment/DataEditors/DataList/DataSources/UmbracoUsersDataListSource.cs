8691c4407fe28f1e9801aa7f3d9d9f27dec0476b
leekelleher <leekelleher@gmail.com>
2021-10-04 19:14:25 +0100

    Data List - added Umbraco Users data-source

---
+++ src/Umbraco.Community.Contentment/DataEditors/DataList/DataSources/UmbracoUsersDataListSource.cs
@@ -0,0 +1,108 @@
+﻿/* Copyright © 2021 Lee Kelleher.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using Umbraco.Core;
+using Umbraco.Core.IO;
+using Umbraco.Core.Models.Membership;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Services;
+using UmbConstants = Umbraco.Core.Constants;
+
+namespace Umbraco.Community.Contentment.DataEditors
+{
+    public sealed class UmbracoUsersDataListSource : IDataListSource, IDataListSourceValueConverter
+    {
+        private readonly IUserService _userService;
+
+        public UmbracoUsersDataListSource(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public string Name => "Umbraco Users";
+
+        public string Description => "Use Umbraco users to populate the data source.";
+
+        public string Icon => UmbConstants.Icons.User;
+
+        public string Group => Constants.Conventions.DataSourceGroups.Umbraco;
+
+        public IEnumerable<ConfigurationField> Fields
+        {
+            get
+            {
+                var items = _userService
+                    .GetAllUserGroups()
+                    .Select(x => new DataListItem
+                    {
+                        Name = x.Name,
+                        Value = x.Alias,
+                        Icon = x.Icon ?? UmbConstants.Icons.UserGroup,
+                        Description = string.Join(", ", x.AllowedSections)
+                    })
+                    .ToList();
+
+                return new[]
+                {
+                    new ConfigurationField
+                    {
+                        Key = "userGroup",
+                        Name = "User Group",
+                        Description = "Select a user group to filter the users by. If left empty, all users will be used.",
+                        View = ItemPickerDataListEditor.DataEditorViewPath,
+                        Config = new Dictionary<string, object>
+                        {
+                            { "enableFilter", items.Count > 5 ? Constants.Values.True : Constants.Values.False },
+                            { "items", items },
+                            { "listType", "list" },
+                            { "overlayView", IOHelper.ResolveUrl(ItemPickerDataListEditor.DataEditorOverlayViewPath) },
+                            { "maxItems", 1 },
+                        }
+                    }
+                };
+            }
+        }
+
+        public Dictionary<string, object> DefaultValues => default;
+
+        public OverlaySize OverlaySize => OverlaySize.Small;
+
+        public IEnumerable<DataListItem> GetItems(Dictionary<string, object> config)
+        {
+            DataListItem mapUser(IUser user)
+            {
+                return new DataListItem
+                {
+                    Name = user.Name,
+                    Value = user.Username,
+                    Icon = Icon,
+                    Description = user.Username,
+                };
+            };
+
+            if (config.TryGetValueAs("userGroup", out JArray array) == true &&
+                array.Count > 0 &&
+                array[0].Value<string>() is string alias &&
+                string.IsNullOrWhiteSpace(alias) == false)
+            {
+                var userGroup = _userService.GetUserGroupByAlias(alias);
+                if (userGroup != null)
+                {
+                    return _userService.GetAllInGroup(userGroup.Id).Select(mapUser);
+                }
+            }
+
+            return _userService.GetAll(0, int.MaxValue, out _).Select(mapUser);
+        }
+
+        public Type GetValueType(Dictionary<string, object> config) => typeof(IUser);
+
+        public object ConvertValue(Type type, string value) => _userService.GetByUsername(value);
+    }
+}
