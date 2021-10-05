f83bbb1effe2568d3db1e9e0ac9b2ae7e473e2af
leekelleher <leekelleher@gmail.com>
2021-10-05 07:19:07 +0100

    Added `HasValueFor` IPublishedElement extension method
    This is useful (when using ModelsBuilder) for checking if a property
    has a value that typically uses an object-type that isn't so easy to
    check if it's empty.
    
    e.g. a rich-text editor will use an `IHtmlString` object-type, if the
    value is empty, the object-value itself isn't `null`, but contains no
    contents. If you wanted to wrap additional markup around the rich-text
    contents, you would need to add extra logic to check for empty contents.
    But now you can use this... `Model.HasValueFor(x => x.BodyText)`

---
+++ src/Umbraco.Community.Contentment/Web/Extensions/PublishedElementExtensions.cs
@@ -0,0 +1,40 @@
+﻿/* Copyright © 2021 Lee Kelleher.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.ModelsBuilder.Embedded;
+
+namespace Umbraco.Web
+{
+    public static class PublishedElementExtensions
+    {
+        public static bool HasValueFor<TModel, TValue>(this TModel model, Expression<Func<TModel, TValue>> property, string culture = null, string segment = null)
+            where TModel : IPublishedElement
+        {
+            var alias = GetAlias(model, property);
+            return model.HasValue(alias, culture, segment);
+        }
+
+        // NOTE: Bah! `PublishedElementExtensions.GetAlias` is marked as private! It's either copy code, or reflection - here we go!
+        // https://github.com/umbraco/Umbraco-CMS/blob/release-8.14.0/src/Umbraco.ModelsBuilder.Embedded/PublishedElementExtensions.cs#L28
+        private static string GetAlias<TModel, TValue>(TModel model, Expression<Func<TModel, TValue>> property)
+        {
+            try
+            {
+                var assembly = typeof(ApiVersion).Assembly;
+                var type = assembly.GetType("Umbraco.Web.PublishedElementExtensions");
+                var method = type.GetMethod(nameof(GetAlias), BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.InvokeMethod);
+                var generic = method.MakeGenericMethod(typeof(TModel), typeof(TValue));
+                return generic.Invoke(null, new object[] { model, property }) as string;
+            }
+            catch { /* ಠ_ಠ */ }
+
+            return default;
+        }
+    }
+}
