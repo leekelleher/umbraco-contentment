cb0cfa48be63fb1d848850e4c9af857af1ed693a
leekelleher <leekelleher@gmail.com>
2022-02-23 17:44:41 +0000

    Added an Umbraco v8 version of the demo website.

---
+++ src/Umbraco.Cms.8.x/App_Code/DataSources/RenderPosition.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Community.Contentment.DataEditors;
+
+namespace Umbraco.Web.PublishedModels
+{
+    public enum RenderPosition
+    {
+        [DataListItem(Name = "Head", Description = "Inside the &lt;head&gt; tags")]
+        Head = 2,
+
+        [DataListItem(Name = "Body (start)", Description = "After the opening &lt;body&gt; tag")]
+        BodyOpen = 1,
+
+        [DataListItem(Name = "Body (end)", Description = "Before the closing &lt;/body&gt; tag")]
+        BodyClose = 0
+    }
+}
---
+++ src/Umbraco.Cms.8.x/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="Umbraco.Web.UmbracoApplication" Language="C#" %>
