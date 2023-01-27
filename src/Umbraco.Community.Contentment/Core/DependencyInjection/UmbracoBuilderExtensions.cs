f979e8f8df304628f72db01c0ee5afca1c8e3f1c
leekelleher <leekelleher@gmail.com>
2023-01-27 14:11:57 +0000

    :shushing_face: `AddUmbracoConfiguration` extension method
    I keep finding that I need to do ad-hoc/conditional configurations
    in the Startup.cs `ConfigureServices()` method. I toyed with adding
    this previously, but it was frowned upon by some, but I keep coming
    back to including it in my implementation projects, so decided to
    include with Contentment.

---
+++ src/Umbraco.Community.Contentment/Core/DependencyInjection/UmbracoBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿/* Copyright © 2023 Lee Kelleher.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#if NET472 == false
+using System;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Umbraco.Extensions
+{
+    public static partial class UmbracoBuilderExtensions
+    {
+        public static IUmbracoBuilder AddUmbracoConfiguration(this IUmbracoBuilder builder, Action<IUmbracoBuilder> action)
+        {
+            action?.Invoke(builder);
+
+            return builder;
+        }
+    }
+}
+#endif
