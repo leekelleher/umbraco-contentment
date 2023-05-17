5347859735dbff3382d1a4714dc945f9ca33fc96
leekelleher <leekelleher@gmail.com>
2023-05-17 20:40:27 +0100

    Adds v8 polyfills for `RawValueProperty` and `PublishedPropertyBase`
    In Umbraco v12, an interface update on `IPublishedProperty`,
    adding a `GetDeliveryApiValue` method causes a breaking-change with
    Contentment's `DetachedPublishedProperty` class.
    
    Reviewing Umbraco core code, I found that the `RawValueProperty` class
    is now public (it has been since v9), but I hadn't reviewed the
    `DetachedPublishedProperty` when porting to .NET Core, just left it as
    it was.  In Umbraco v8, the `RawValueProperty` was marked as internal,
    again, I hadn't considered using it.
    
    With Contentment's polyfill code, I am able to backport the
    `RawValueProperty` and `PublishedPropertyBase` classes, so to mitigate
    the breaking-change introduced by Umbraco v12.
    
    Fixes #322

---
+++ src/Umbraco.Community.Contentment/Polyfill/Core/Models/PublishedContent/PublishedPropertyBase.cs
@@ -0,0 +1,62 @@
+﻿/* Copyright © 2013-present Umbraco.
+ * This Source Code has been derived from Umbraco CMS.
+ * https://github.com/umbraco/Umbraco-CMS/blob/release-8.18.8/src/Umbraco.Core/Models/PublishedContent/PublishedPropertyBase.cs
+ * Modified under the permissions of the MIT License.
+ * Modifications are licensed under the Mozilla Public License.
+ * Copyright © 2023 Lee Kelleher.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#if NET472
+using System;
+using System.Diagnostics;
+using Umbraco.Core.PropertyEditors;
+
+namespace Umbraco.Core.Models.PublishedContent
+{
+    [DebuggerDisplay("{Alias} ({PropertyType?.EditorAlias})")]
+    internal abstract class PublishedPropertyBase : IPublishedProperty
+    {
+        protected PublishedPropertyBase(IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel)
+        {
+            PropertyType = propertyType ?? throw new ArgumentNullException(nameof(propertyType));
+            ReferenceCacheLevel = referenceCacheLevel;
+
+            ValidateCacheLevel(ReferenceCacheLevel, true);
+            ValidateCacheLevel(PropertyType.CacheLevel, false);
+        }
+
+        private static void ValidateCacheLevel(PropertyCacheLevel cacheLevel, bool validateUnknown)
+        {
+            switch (cacheLevel)
+            {
+                case PropertyCacheLevel.Element:
+                case PropertyCacheLevel.Elements:
+                case PropertyCacheLevel.Snapshot:
+                case PropertyCacheLevel.None:
+                    break;
+                case PropertyCacheLevel.Unknown:
+                    if (!validateUnknown) goto default;
+                    break;
+                default:
+                    throw new Exception($"Invalid cache level \"{cacheLevel}\".");
+            }
+        }
+
+        public IPublishedPropertyType PropertyType { get; }
+
+        public PropertyCacheLevel ReferenceCacheLevel { get; }
+
+        public string Alias => PropertyType.Alias;
+
+        public abstract bool HasValue(string culture = null, string segment = null);
+
+        public abstract object GetSourceValue(string culture = null, string segment = null);
+
+        public abstract object GetValue(string culture = null, string segment = null);
+
+        public abstract object GetXPathValue(string culture = null, string segment = null);
+    }
+}
+#endif
---
+++ src/Umbraco.Community.Contentment/Polyfill/Core/Models/PublishedContent/RawValueProperty.cs
@@ -0,0 +1,54 @@
+﻿/* Copyright © 2013-present Umbraco.
+ * This Source Code has been derived from Umbraco CMS.
+ * https://github.com/umbraco/Umbraco-CMS/blob/release-8.18.8/src/Umbraco.Core/Models/PublishedContent/RawValueProperty.cs
+ * Modified under the permissions of the MIT License.
+ * Modifications are licensed under the Mozilla Public License.
+ * Copyright © 2023 Lee Kelleher.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#if NET472
+using System;
+using Umbraco.Core.PropertyEditors;
+
+namespace Umbraco.Core.Models.PublishedContent
+{
+    internal class RawValueProperty : PublishedPropertyBase
+    {
+        private readonly object _sourceValue;
+        private readonly Lazy<object> _objectValue;
+        private readonly Lazy<object> _xpathValue;
+
+        public RawValueProperty(IPublishedPropertyType propertyType, IPublishedElement content, object sourceValue, bool isPreviewing = false)
+            : base(propertyType, PropertyCacheLevel.Unknown)
+        {
+            if (propertyType.Variations != ContentVariation.Nothing)
+            {
+                throw new ArgumentException("Property types with variations are not supported here.", nameof(propertyType));
+            }
+
+            _sourceValue = sourceValue;
+
+            var interValue = new Lazy<object>(() => PropertyType.ConvertSourceToInter(content, _sourceValue, isPreviewing));
+            _objectValue = new Lazy<object>(() => PropertyType.ConvertInterToObject(content, PropertyCacheLevel.Unknown, interValue.Value, isPreviewing));
+            _xpathValue = new Lazy<object>(() => PropertyType.ConvertInterToXPath(content, PropertyCacheLevel.Unknown, interValue.Value, isPreviewing));
+        }
+
+        public override object GetSourceValue(string culture = null, string segment = null)
+            => string.IsNullOrEmpty(culture) & string.IsNullOrEmpty(segment) ? _sourceValue : null;
+
+        public override bool HasValue(string culture = null, string segment = null)
+        {
+            var sourceValue = GetSourceValue(culture, segment);
+            return sourceValue is string s ? !string.IsNullOrWhiteSpace(s) : sourceValue != null;
+        }
+
+        public override object GetValue(string culture = null, string segment = null)
+            => string.IsNullOrEmpty(culture) & string.IsNullOrEmpty(segment) ? _objectValue.Value : null;
+
+        public override object GetXPathValue(string culture = null, string segment = null)
+            => string.IsNullOrEmpty(culture) & string.IsNullOrEmpty(segment) ? _xpathValue.Value : null;
+    }
+}
+#endif
