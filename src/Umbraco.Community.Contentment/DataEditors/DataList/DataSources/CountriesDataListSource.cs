d966d5ef0f6c676fd0d607f7b640451c7d754f3a
leekelleher <leekelleher@gmail.com>
2023-05-25 11:35:15 +0100

    Refactored the .NET data-sources to support DataPicker

--- src/Umbraco.Community.Contentment/DataEditors/DataList/DataSources/CountriesDataListSource.cs
+++ src/Umbraco.Community.Contentment/DataEditors/DataList/DataSources/CountriesDataListSource.cs
@@ -6,17 +6,20 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Threading.Tasks;
 #if NET472
 using Umbraco.Core;
+using Umbraco.Core.Models;
 using Umbraco.Core.PropertyEditors;
 #else
+using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.PropertyEditors;
 using Umbraco.Extensions;
 #endif
 
 namespace Umbraco.Community.Contentment.DataEditors
 {
-    public sealed class CountriesDataListSource : IDataListSource
+    public sealed class CountriesDataListSource : IDataListSource, IDataPickerSource
     {
         public string Name => ".NET Countries (ISO 3166-1)";
 
@@ -34,21 +37,80 @@ namespace Umbraco.Community.Contentment.DataEditors
 
         public IEnumerable<DataListItem> GetItems(Dictionary<string, object> config)
         {
+            return GetRegions()
+                .OrderBy(x => x.EnglishName)
+                .Select(ToDataListItem);
+        }
+
+        public Task<IEnumerable<DataListItem>> GetItemsAsync(Dictionary<string, object> config, IEnumerable<string> values)
+        {
+            if (values?.Any() == true)
+            {
+                return Task.FromResult(values
+                    .Select(x => new RegionInfo(x))
+                    .Select(ToDataListItem));
+            }
+
+            return Task.FromResult(Enumerable.Empty<DataListItem>());
+        }
+
+        public Task<PagedResult<DataListItem>> SearchAsync(Dictionary<string, object> config, int pageNumber = 1, int pageSize = 12, string query = "")
+        {
+            var items = default(IEnumerable<DataListItem>);
+
+            if (string.IsNullOrWhiteSpace(query) == true)
+            {
+                items = GetItems(config);
+            }
+            else
+            {
+                items = GetRegions()
+                    .Where(x => x.EnglishName.InvariantContains(query) == true || x.TwoLetterISORegionName.InvariantStartsWith(query) == true)
+                    .OrderBy(x => x.EnglishName)
+                    .Select(ToDataListItem);
+            }
+
+            if (items?.Any() == true)
+            {
+                var offset = (pageNumber - 1) * pageSize;
+                var results = new PagedResult<DataListItem>(items.Count(), pageNumber, pageSize)
+                {
+                    Items = items.Skip(offset).Take(pageSize)
+                };
+
+                return Task.FromResult(results);
+            }
+
+            return Task.FromResult(new PagedResult<DataListItem>(-1, pageNumber, pageSize));
+        }
+
+        private IEnumerable<RegionInfo> GetRegions()
+        {
             return CultureInfo
-                .GetCultures(CultureTypes.SpecificCultures)
+               .GetCultures(CultureTypes.SpecificCultures)
 #if NET7_0_OR_GREATER
                 .Select(x => new RegionInfo(x.Name))
 #else
-                .Select(x => new RegionInfo(x.LCID))
+               .Select(x => new RegionInfo(x.LCID))
 #endif
-                .DistinctBy(x => x.TwoLetterISORegionName)
-                .Where(x => x.TwoLetterISORegionName.Length == 2) // NOTE: Removes odd "countries" such as Caribbean (029), Europe (150), Latin America (419) and World (001).
-                .OrderBy(x => x.EnglishName)
-                .Select(x => new DataListItem
-                {
-                    Name = x.EnglishName,
-                    Value = x.TwoLetterISORegionName
-                });
+               .DistinctBy(x => x.TwoLetterISORegionName)
+               // NOTE: Removes odd "countries" such as Caribbean (029), Europe (150), Latin America (419) and World (001).
+               .Where(x => x.TwoLetterISORegionName.Length == 2);
+        }
+
+        private DataListItem ToDataListItem(RegionInfo region)
+        {
+            return new DataListItem
+            {
+                Name = region.EnglishName,
+                Value = region.TwoLetterISORegionName,
+                Icon = "icon-globe",
+                Description = region.TwoLetterISORegionName,
+                //Properties = new Dictionary<string, object>
+                //{
+                //    { "image", $"https://flagcdn.com/256x192/{region.TwoLetterISORegionName.ToLowerInvariant()}.png" },
+                //},
+            };
         }
     }
 }
--- src/Umbraco.Community.Contentment/DataEditors/DataList/DataSources/LanguagesDataListSource.cs
+++ src/Umbraco.Community.Contentment/DataEditors/DataList/DataSources/LanguagesDataListSource.cs
@@ -3,20 +3,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Threading.Tasks;
 #if NET472
 using Umbraco.Core;
+using Umbraco.Core.Models;
 using Umbraco.Core.PropertyEditors;
 #else
+using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.PropertyEditors;
 using Umbraco.Extensions;
 #endif
 
 namespace Umbraco.Community.Contentment.DataEditors
 {
-    public sealed class LanguagesDataListSource : IDataListSource
+    public sealed class LanguagesDataListSource : IDataListSource, IDataPickerSource
     {
         public string Name => ".NET Languages (ISO 639-1)";
 
@@ -34,16 +38,74 @@ namespace Umbraco.Community.Contentment.DataEditors
 
         public IEnumerable<DataListItem> GetItems(Dictionary<string, object> config)
         {
+            return GetCultures()
+                .OrderBy(x => x.EnglishName)
+                .Select(ToDataListItem);
+        }
+
+        public Task<IEnumerable<DataListItem>> GetItemsAsync(Dictionary<string, object> config, IEnumerable<string> values)
+        {
+            if (values?.Any() == true)
+            {
+                var lookup = GetCultures().ToLookup(x => x.TwoLetterISOLanguageName, StringComparer.InvariantCultureIgnoreCase);
+
+                return Task.FromResult(values
+                    .Where(x => lookup.Contains(x) == true)
+                    .SelectMany(x => lookup[x])
+                    .Select(ToDataListItem));
+            }
+
+            return Task.FromResult(Enumerable.Empty<DataListItem>());
+        }
+
+        public Task<PagedResult<DataListItem>> SearchAsync(Dictionary<string, object> config, int pageNumber = 1, int pageSize = 12, string query = "")
+        {
+            var items = default(IEnumerable<DataListItem>);
+
+            if (string.IsNullOrWhiteSpace(query) == true)
+            {
+                items = GetItems(config);
+            }
+            else
+            {
+                items = GetCultures()
+                    .Where(x => x.EnglishName.InvariantContains(query) == true || x.TwoLetterISOLanguageName.InvariantStartsWith(query) == true)
+                    .OrderBy(x => x.EnglishName)
+                    .Select(ToDataListItem);
+            }
+
+            if (items?.Any() == true)
+            {
+                var offset = (pageNumber - 1) * pageSize;
+                var results = new PagedResult<DataListItem>(items.Count(), pageNumber, pageSize)
+                {
+                    Items = items.Skip(offset).Take(pageSize)
+                };
+
+                return Task.FromResult(results);
+            }
+
+            return Task.FromResult(new PagedResult<DataListItem>(-1, pageNumber, pageSize));
+        }
+
+        private IEnumerable<CultureInfo> GetCultures()
+        {
             return CultureInfo
                 .GetCultures(CultureTypes.AllCultures)
                 .DistinctBy(x => x.TwoLetterISOLanguageName)
-                .Where(x => x.TwoLetterISOLanguageName.Length == 2) // NOTE: Removes any odd languages.
-                .OrderBy(x => x.EnglishName)
-                .Select(x => new DataListItem
-                {
-                    Name = x.EnglishName,
-                    Value = x.TwoLetterISOLanguageName
-                });
+                // NOTE: Removes any odd languages.
+                .Where(x => x.TwoLetterISOLanguageName.Length == 2);
+        }
+
+        private DataListItem ToDataListItem(CultureInfo culture)
+        {
+            return new DataListItem
+            {
+                Name = culture.EnglishName,
+                Value = culture.TwoLetterISOLanguageName,
+                Icon = "icon-umb-translation",
+                Description = culture.TwoLetterISOLanguageName,
+            };
         }
     }
 }
