2acc04ea354c42462f13ee19197633345567c3c2
leekelleher <leekelleher@gmail.com>
2023-11-02 23:36:54 +0000

    Upgraded Umbraco dependency to v13-rc1
    Added v13 test site.

---
+++ src/Umbraco.Cms.13.x/DataSources/ContentmentContentBlocksDataListItemPropertyValueConverter.cs
@@ -0,0 +1,39 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Strings;
+using Umbraco.Community.Contentment.DataEditors;
+
+namespace Umbraco.Community.Contentment.DataSources
+{
+    public class ContentmentContentBlocksDataListItemPropertyValueConverter : IDataListItemPropertyValueConverter
+    {
+        private readonly IShortStringHelper _shortStringHelper;
+
+        public ContentmentContentBlocksDataListItemPropertyValueConverter(IShortStringHelper shortStringHelper)
+        {
+            _shortStringHelper = shortStringHelper;
+        }
+
+        public bool IsConverter(IPublishedPropertyType propertyType)
+        {
+            return propertyType.EditorAlias == "Umbraco.Community.Contentment.ContentBlocks";
+        }
+
+        public IEnumerable<DataListItem> ConvertTo(IPublishedProperty property)
+        {
+            var value = property.GetValue() as List<IPublishedElement>;
+
+            if (value?.Any() == true)
+            {
+                return value.Select(x => new DataListItem
+                {
+                    Name = x.ContentType.Alias.SplitPascalCasing(_shortStringHelper).ToFriendlyName(),
+                    Value = x.Key.ToString(),
+                    Icon = x.ContentType.GetIcon(),
+                    Description = string.Join(", ", x.Properties.Where(p => p.PropertyType.ModelClrType == typeof(string)).Select(p => p.GetValue()?.ToString()?.StripHtml())),
+                });
+            }
+
+            return Enumerable.Empty<DataListItem>();
+        }
+    }
+}
