755d4d0588e3ff7d0571c7c936056bf848f4102b
leekelleher <leekelleher@gmail.com>
2023-11-09 12:56:48 +0000

    [WIP] Code Editor

--- src/Umbraco.Community.Contentment/DataEditors/manifests.ts
+++ src/Umbraco.Community.Contentment/DataEditors/manifests.ts
@@ -3,10 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import { codeEditorUiManifest } from "./CodeEditor/code-editor.manifest.js";
 import { editorNotesUiManifest } from "./EditorNotes/editor-notes.manifest.js";
 import { notesSchemaManifest, notesUiManifest } from "./Notes/notes.manifest.js";
 
 export const manifests = [
+    codeEditorUiManifest,
     editorNotesUiManifest,
     notesSchemaManifest,
     notesUiManifest
