222c7b34f737987de25a5f070caaabd3df18fb3b
leekelleher <leekelleher@gmail.com>
2023-12-23 14:17:37 +0000

    RichTextEditor - consolidated the view path
    To reduce the multi-targeting preprocessor directive usage.

--- src/Umbraco.Community.Contentment/DataEditors/RichTextEditor/RichTextEditorDataEditor.cs
+++ src/Umbraco.Community.Contentment/DataEditors/RichTextEditor/RichTextEditorDataEditor.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright © 2023 Lee Kelleher.
+/* Copyright © 2023 Lee Kelleher.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
@@ -7,6 +7,10 @@ namespace Umbraco.Community.Contentment.DataEditors
 {
     internal sealed class RichTextEditorDataEditor
     {
+#if NET8_0_OR_GREATER
         internal const string DataEditorViewPath = Constants.Internals.EditorsPathRoot + "rich-text-editor.html";
+#else
+        internal const string DataEditorViewPath = "~/umbraco/views/propertyeditors/rte/rte.html";
+#endif
     }
 }
