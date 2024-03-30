5f6e4a49ae404d20a270aa69e676fea970acf961
leekelleher <leekelleher@gmail.com>
2024-03-30 11:56:18 +0000

    Composer: adds a pipeline filter
    to add conditional middleware for BlockList's `GetEmptyByKeys()` call,
    to ensure that the `Request.Body` is still accessible after the initial binding.

--- src/Umbraco.Community.Contentment/Composing/ContentmentComposer.cs
+++ src/Umbraco.Community.Contentment/Composing/ContentmentComposer.cs
@@ -1,12 +1,15 @@
-﻿/* Copyright © 2019 Lee Kelleher.
+/* Copyright © 2019 Lee Kelleher.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Web.Common.ApplicationBuilder;
 using Umbraco.Community.Contentment.DataEditors;
 using Umbraco.Community.Contentment.Notifications;
 using Umbraco.Community.Contentment.Services;
@@ -47,6 +50,20 @@ namespace Umbraco.Community.Contentment.Composing
                 .AddNotificationHandler<ServerVariablesParsingNotification, ContentmentServerVariablesParsingNotification>()
                 .AddNotificationHandler<UmbracoApplicationStartingNotification, ContentmentUmbracoApplicationStartingNotification>()
             ;
+
+            builder.Services.Configure<UmbracoPipelineOptions>(opts =>
+            {
+                opts.AddFilter(new UmbracoPipelineFilter("UmbBlockListGetEmptyByKeysRequest_EnableBuffering")
+                {
+                    // HACK:  [LK] To support `IContentmentContentContext` inside the BlockList editor,
+                    // we need to re-access the `Request.Body` to extract the `parentId` value.
+                    // The `GetEmptyByKeys` path has been hard-coded, as unsure how else to generate it at this stage.
+                    PrePipeline = app => app.UseWhen(
+                        ctx => ctx.Request.Path.StartsWithSegments("/umbraco/backoffice/umbracoapi/content/GetEmptyByKeys"),
+                        app2 => app2.Use(async (ctx, next) => { ctx.Request.EnableBuffering(); await next(ctx); })
+                    ),
+                });
+            });
         }
     }
 }
