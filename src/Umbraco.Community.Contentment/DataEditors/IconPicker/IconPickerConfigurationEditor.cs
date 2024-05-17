72753f585c7c794d15d7b399676693ddc5f9b0ab
leekelleher <leekelleher@gmail.com>
2024-05-17 10:44:22 +0100

    IconPicker: removed v11.2 version check

--- src/Umbraco.Community.Contentment/DataEditors/IconPicker/IconPickerConfigurationEditor.cs
+++ src/Umbraco.Community.Contentment/DataEditors/IconPicker/IconPickerConfigurationEditor.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright © 2019 Lee Kelleher.
+/* Copyright © 2019 Lee Kelleher.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
@@ -12,7 +12,7 @@ namespace Umbraco.Community.Contentment.DataEditors
 {
     internal sealed class IconPickerConfigurationEditor : ConfigurationEditor
     {
-        public IconPickerConfigurationEditor(IIOHelper ioHelper, IUmbracoVersion umbracoVersion)
+        public IconPickerConfigurationEditor(IIOHelper ioHelper)
             : base()
         {
             Fields.Add(new ConfigurationField
@@ -48,16 +48,13 @@ namespace Umbraco.Community.Contentment.DataEditors
                 }
             });
 
-            if (umbracoVersion.Version >= new Version(11, 2))
+            Fields.Add(new ConfigurationField
             {
-                Fields.Add(new ConfigurationField
-                {
-                    Key = "hideColors",
-                    Name = "Hide colors?",
-                    Description = "Select to hide the color options in the icon picker panel.",
-                    View = "boolean",
-                });
-            }
+                Key = "hideColors",
+                Name = "Hide colors?",
+                Description = "Select to hide the color options in the icon picker panel.",
+                View = "boolean",
+            });
         }
     }
 }
--- src/Umbraco.Community.Contentment/DataEditors/IconPicker/IconPickerDataEditor.cs
+++ src/Umbraco.Community.Contentment/DataEditors/IconPicker/IconPickerDataEditor.cs
@@ -3,7 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-using Umbraco.Cms.Core.Configuration;
 using Umbraco.Cms.Core.IO;
 using Umbraco.Cms.Core.PropertyEditors;
 
@@ -25,18 +24,15 @@ namespace Umbraco.Community.Contentment.DataEditors
         internal const string DataEditorIcon = "icon-fa fa-circle-o";
 
         private readonly IIOHelper _ioHelper;
-        private readonly IUmbracoVersion _umbracoVersion;
 
         public IconPickerDataEditor(
             IIOHelper ioHelper,
-            IUmbracoVersion umbracoVersion,
             IDataValueEditorFactory dataValueEditorFactory)
             : base(dataValueEditorFactory)
         {
             _ioHelper = ioHelper;
-            _umbracoVersion = umbracoVersion;
         }
 
-        protected override IConfigurationEditor CreateConfigurationEditor() => new IconPickerConfigurationEditor(_ioHelper, _umbracoVersion);
+        protected override IConfigurationEditor CreateConfigurationEditor() => new IconPickerConfigurationEditor(_ioHelper);
     }
 }
