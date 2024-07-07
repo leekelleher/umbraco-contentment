596f0f3f87c0c4167704c929aa933fa977ac8159
leekelleher <leekelleher@gmail.com>
2024-07-07 16:48:41 +0100

    Added missing social network icons

--- src/Umbraco.Community.Contentment/Client/src/icons/icons.ts
+++ src/Umbraco.Community.Contentment/Client/src/icons/icons.ts
@@ -1,9 +1,15 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright © 2024 Lee Kelleher
 
+const iconPath = '/App_Plugins/Contentment/icons';
+
 export default [
-	{
-		name: 'icon-contentment',
-		path: '/App_Plugins/Contentment/icons/icon-contentment.js',
-	},
+	{ name: 'icon-bluesky', path: `${iconPath}/icon-bluesky.js` },
+	{ name: 'icon-contentment', path: `${iconPath}/icon-contentment.js` },
+	{ name: 'icon-discord', path: `${iconPath}/icon-discord.js` },
+	{ name: 'icon-instagram', path: `${iconPath}/icon-instagram.js` },
+	{ name: 'icon-threads', path: `${iconPath}/icon-threads.js` },
+	{ name: 'icon-tiktok', path: `${iconPath}/icon-tiktok.js` },
+	{ name: 'icon-x-twitter', path: `${iconPath}/icon-x-twitter.js` },
+	{ name: 'icon-youtube', path: `${iconPath}/icon-youtube.js` },
 ];
