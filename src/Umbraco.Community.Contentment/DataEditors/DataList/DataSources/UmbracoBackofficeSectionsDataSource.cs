401dd31d83e2985fb00c9c51dffad2065d90c777
leekelleher <leekelleher@gmail.com>
2024-08-04 17:23:20 +0100

    Reinstated "Umbraco Backoffice Sections" data-source
    it doesn't do anything, just there to prevent an UI error/bug.

--- src/Umbraco.Community.Contentment/DataEditors/DataList/DataSources/UmbracoBackofficeSectionsDataSource.cs
+++ src/Umbraco.Community.Contentment/DataEditors/DataList/DataSources/UmbracoBackofficeSectionsDataSource.cs
@@ -1,52 +1,28 @@
-///* Copyright © 2023 Lee Kelleher.
-// * This Source Code Form is subject to the terms of the Mozilla Public
-// * License, v. 2.0. If a copy of the MPL was not distributed with this
-// * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+/* Copyright © 2023 Lee Kelleher.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-//using Umbraco.Cms.Core.PropertyEditors;
-//using Umbraco.Cms.Core.Sections;
-//using Umbraco.Cms.Core.Services;
-//using Umbraco.Extensions;
+using Umbraco.Cms.Core.PropertyEditors;
 
-//namespace Umbraco.Community.Contentment.DataEditors
-//{
-//    public sealed class UmbracoBackofficeSectionsDataSource : DataListToDataPickerSourceBridge, IDataListSource, IDataSourceValueConverter
-//    {
-//        private readonly ISectionService _sectionService;
+namespace Umbraco.Community.Contentment.DataEditors
+{
+    public sealed class UmbracoBackofficeSectionsDataSource : DataListToDataPickerSourceBridge, IDataListSource
+    {
+        public override string Name => "Umbraco Backoffice Sections";
 
-//        public UmbracoBackofficeSectionsDataSource(ISectionService sectionService)
-//        {
-//            _sectionService = sectionService;
-//        }
+        public override string Description => "Use the backoffice sections to populate the data source.";
 
-//        public override string Name => "Umbraco Backoffice Sections";
+        public override string Icon => "icon-section";
 
-//        public override string Description => "Use the backoffice sections to populate the data source.";
+        public override string Group => Constants.Conventions.DataSourceGroups.Umbraco;
 
-//        public override string Icon => "icon-section";
+        public override IEnumerable<ContentmentConfigurationField> Fields => [];
 
-//        public override string Group => Constants.Conventions.DataSourceGroups.Umbraco;
+        public override Dictionary<string, object>? DefaultValues => default;
 
-//        public override IEnumerable<ContentmentConfigurationField> Fields => Enumerable.Empty<ContentmentConfigurationField>();
+        public override OverlaySize OverlaySize => OverlaySize.Small;
 
-//        public override Dictionary<string, object>? DefaultValues => default;
-
-//        public override OverlaySize OverlaySize => OverlaySize.Small;
-
-//        public override IEnumerable<DataListItem> GetItems(Dictionary<string, object> config)
-//        {
-//            return _sectionService
-//                .GetSections()
-//                .Select(x => new DataListItem
-//                {
-//                    Name = x.Name,
-//                    Value = x.Alias,
-//                    Icon = Icon,
-//                });
-//        }
-
-//        public Type? GetValueType(Dictionary<string, object>? config) => typeof(ISection);
-
-//        public object? ConvertValue(Type type, string value) => _sectionService.GetByAlias(value);
-//    }
-//}
+        public override IEnumerable<DataListItem> GetItems(Dictionary<string, object> config) => [];
+    }
+}
