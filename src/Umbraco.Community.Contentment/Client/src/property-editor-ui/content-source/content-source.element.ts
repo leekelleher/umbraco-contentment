e607af93a55441f6044050ab5bf43c79c038d805
leekelleher <leekelleher@gmail.com>
2024-08-04 17:22:21 +0100

    Reuses Umbraco's `input-content-picker-source` component
    for selecting the Content Source.

--- src/Umbraco.Community.Contentment/Client/src/property-editor-ui/content-source/content-source.element.ts
+++ src/Umbraco.Community.Contentment/Client/src/property-editor-ui/content-source/content-source.element.ts
@@ -8,6 +8,7 @@ import {
 	UmbPropertyEditorConfigCollection,
 	UmbPropertyValueChangeEvent,
 } from '@umbraco-cms/backoffice/property-editor';
+import { UMB_APP_CONTEXT } from '@umbraco-cms/backoffice/app';
 
 const ELEMENT_NAME = 'contentment-property-editor-ui-content-source';
 
@@ -16,11 +17,27 @@ export class ContentmentPropertyEditorUIContentSourceElement
 	extends UmbLitElement
 	implements UmbPropertyEditorUiElement
 {
+	#umbBackofficePath = '/umbraco';
+
 	@property({ type: Object })
 	public value?: unknown;
 
 	public set config(config: UmbPropertyEditorConfigCollection | undefined) {
 		if (!config) return;
+	}
+
+	constructor() {
+		super();
+
+		this.consumeContext(UMB_APP_CONTEXT, (appContext) => {
+			this.#umbBackofficePath = appContext.getBackofficePath();
+		});
+	}
+
+	override async firstUpdated() {
+		const path =
+			'/backoffice/packages/property-editors/content-picker/config/source/input-content-picker-source/input-content-picker-source.element.js';
+		await import(this.#umbBackofficePath + path);
 	}
 
 	#onChange(event: CustomEvent & { target: { data: unknown } }) {
