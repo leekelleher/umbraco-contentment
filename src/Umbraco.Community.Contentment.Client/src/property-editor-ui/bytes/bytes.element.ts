23fb9acf832c99e645b0bd0414e33e2457467963
leekelleher <leekelleher@gmail.com>
2025-11-03 00:30:49 +0000

    Bytes: refactored to use core's `formatBytes` function
    which was derived from Contentment.

--- src/Umbraco.Community.Contentment.Client/src/property-editor-ui/bytes/bytes.element.ts
+++ src/Umbraco.Community.Contentment.Client/src/property-editor-ui/bytes/bytes.element.ts
@@ -2,16 +2,14 @@
 // Copyright © 2024 Lee Kelleher
 
 import { customElement, property, state } from '@umbraco-cms/backoffice/external/lit';
+import { formatBytes } from '@umbraco-cms/backoffice/utils';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import type { UmbPropertyEditorUiElement } from '@umbraco-cms/backoffice/property-editor';
 
 @customElement('contentment-property-editor-ui-bytes')
 export class ContentmentPropertyEditorUIBytesElement extends UmbLitElement implements UmbPropertyEditorUiElement {
 	@state()
-	private _decimals = 0;
-
-	@state()
-	private _kilo = 1024;
+	private _options = { decimals: 0, kilo: 1024 };
 
 	@property({ type: Number })
 	public value: number = 0;
@@ -20,26 +18,14 @@ export class ContentmentPropertyEditorUIBytesElement extends UmbLitElement imple
 		if (!config) return;
 
 		const decimals = config.getValueByAlias<{ from: number; to: number }>('decimals');
-		this._decimals = decimals?.from && decimals.from > 0 ? decimals.from : 2;
+		this._options.decimals = decimals?.from && decimals.from > 0 ? decimals.from : 2;
 
 		const kilo = Number(config.getValueByAlias('kilo'));
-		this._kilo = kilo > 0 ? kilo : 1024;
-	}
-
-	#formatBytes(bytes?: number): string {
-		if (!bytes) return '0 Bytes';
-
-		const k = this._kilo || 1024;
-		const dm = this._decimals || 0;
-		const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-
-		const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-		return Number.parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+		this._options.kilo = kilo > 0 ? kilo : 1024;
 	}
 
 	override render() {
-		return this.#formatBytes(this.value);
+		return formatBytes(this.value, this._options);
 	}
 }
 
