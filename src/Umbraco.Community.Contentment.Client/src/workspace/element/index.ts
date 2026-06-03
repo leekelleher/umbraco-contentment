18ae547a600fa6bd79ff903487562711c5a04a5c
leekelleher <leekelleher@gmail.com>
2026-06-03 18:15:09 +0100

    feat: register element workspace modal manifest

---
+++ src/Umbraco.Community.Contentment.Client/src/workspace/element/index.ts
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+// Copyright © 2025 Lee Kelleher
+
+export * from './element-manager.context.js';
+export * from './element-property-dataset.context.js';
+export * from './element-workspace-modal.token.js';
+export type * from './types.js';
