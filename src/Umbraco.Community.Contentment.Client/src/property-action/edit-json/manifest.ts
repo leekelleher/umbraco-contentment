9c39f4284f8d91be5aa7ce7a7c4bfaa57380d465
leekelleher <leekelleher@gmail.com>
2026-06-05 17:14:48 +0100

    Edit JSON property-action: adds `RadioButtonList` to the list

--- src/Umbraco.Community.Contentment.Client/src/property-action/edit-json/manifest.ts
+++ src/Umbraco.Community.Contentment.Client/src/property-action/edit-json/manifest.ts
@@ -20,6 +20,7 @@ export const manifest: UmbExtensionManifest = {
 		'Umb.Contentment.PropertyEditorUi.InputList',
 		'Umb.Contentment.PropertyEditorUi.ListItems',
 		'Umb.Contentment.PropertyEditorUi.Notes',
+		'Umb.Contentment.PropertyEditorUi.RadioButtonList',
 		'Umb.Contentment.PropertyEditorUi.ReadOnly',
 		'Umb.Contentment.PropertyEditorUi.SocialLinks',
 		'Umb.Contentment.PropertyEditorUi.TemplatedLabel',
