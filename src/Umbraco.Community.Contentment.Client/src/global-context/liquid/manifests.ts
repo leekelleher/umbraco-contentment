da9e615c4a40642e65f3fb8a3712c94444179d95
leekelleher <leekelleher@gmail.com>
2026-06-05 17:14:25 +0100

    Eager load global-context and conditions
    as they are requested on app startup,
    so this reduces the number of requests.

--- src/Umbraco.Community.Contentment.Client/src/global-context/liquid/manifests.ts
+++ src/Umbraco.Community.Contentment.Client/src/global-context/liquid/manifests.ts
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
 // Copyright © 2025 Lee Kelleher
 
+import ContentmentLiquidContext from './liquid.context.js';
+
 export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'globalContext',
 		alias: 'Umb.Contentment.GlobalContext.Liquid',
 		name: '[Contentment] Liquid Global Context',
-		api: () => import('./liquid.context.js'),
+		api: ContentmentLiquidContext,
 	},
 ];
