2bae0bba493555a4e7e055e2e8fb5a76586723a4
leekelleher <leekelleher@gmail.com>
2026-08-16 10:05:32 +0100

    Data List: altered the contentType observable
    to reduce the scope of the observation,
    (only on `unique` property not the whole object).

--- src/Umbraco.Community.Contentment.Client/src/property-editor-ui/data-list/data-list.element.ts
+++ src/Umbraco.Community.Contentment.Client/src/property-editor-ui/data-list/data-list.element.ts
@@ -53,7 +53,7 @@ export class ContentmentPropertyEditorUIDataListElement
 	private _listEditor?: ContentmentConfigurationEditorValue;
 
 	@state()
-    private _contentTypeUnique?: string | null;
+	private _contentTypeUnique?: string | null;
 
 	@property({ type: Boolean })
 	mandatory = false;
@@ -81,7 +81,10 @@ export class ContentmentPropertyEditorUIDataListElement
 		this.consumeContext(UMB_CONTENT_WORKSPACE_CONTEXT, (contentWorkspaceContext) => {
 			this.observe(contentWorkspaceContext?.isNew, (isNew) => (this._entityIsNew = isNew ?? false));
 			this.observe(contentWorkspaceContext?.unique, (unique) => (this._entityUnique = unique));
-			this.observe(contentWorkspaceContext?.structure.ownerContentType, (contentType) => (this._contentTypeUnique = contentType?.unique));
+			this.observe(
+				contentWorkspaceContext?.structure.ownerContentTypeObservablePart((x) => x?.unique),
+				(unique) => (this._contentTypeUnique = unique),
+			);
 		}).passContextAliasMatches();
 
 		this.consumeContext(UMB_PARENT_ENTITY_CONTEXT, (parentEntityContext) => {
