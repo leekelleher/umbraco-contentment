68b060a0cd16bef07fa4a2014fe871e1bf9fda7c
Laura Weatherhead <laura@spun.dev>
2026-09-01 19:18:12 +0100

    Hide uuiBox in the case that no properties are visible within it

--- src/Umbraco.Community.Contentment.Client/src/utils/move-before-property-group.function.ts
+++ src/Umbraco.Community.Contentment.Client/src/utils/move-before-property-group.function.ts
@@ -9,7 +9,7 @@ export function tryMoveBeforePropertyGroup(element: UmbPropertyEditorUiElement):
 		const umbProperty = closest('umb-property', element) as HTMLElement;
 		if (!umbProperty) return;
 
-		const uuiBox = closest('uui-box', umbProperty);
+		const uuiBox = closest('uui-box', umbProperty) as HTMLElement;
 		if (!uuiBox) return;
 
 		const umbContentWorkspaceViewEditTab = closest('umb-content-workspace-view-edit-tab', uuiBox);
@@ -17,5 +17,13 @@ export function tryMoveBeforePropertyGroup(element: UmbPropertyEditorUiElement):
 
 		umbContentWorkspaceViewEditTab.shadowRoot?.insertBefore(element, uuiBox);
 		umbProperty.style.display = 'none';
+
+		const hasVisibleProperty = Array.from(uuiBox.querySelectorAll('umb-property')).some(
+			(property) => (property as HTMLElement).style.display !== 'none',
+		);
+
+		if (!hasVisibleProperty) {
+			uuiBox.style.display = 'none';
+		}
 	}
 }
